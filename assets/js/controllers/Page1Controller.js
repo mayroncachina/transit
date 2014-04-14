bebf3ee695148e64e2148c4a827c717039f6935a
Mayron Cachina <mayroncachina@gmail.com>
2014-04-14 17:46:10 -0300

    all

--- assets/js/controllers/Page1Controller.js
+++ assets/js/controllers/Page1Controller.js
@@ -7,6 +7,65 @@ Page1Controller.prototype = {
 		self = this;
 		//scroll listener
 		//FG.$scrollApp.addEventListener("scroll", self.onScroll);
+
+
+		$(document).delegate('.multar', 'click', function(e) {
+		    
+			var placa = $("input[name='letras']").val()+'-'+$("input[name='numeros']").val();
+			var latitude = $("input[name='latitude']").val();
+			var longitude = $("input[name='longitude']").val();
+			var condutor = $("input[name='condutor']").val();
+			var infracao = $("select[name='infracao']").val();
+			var imagem = $("#imagem").attr('src');
+
+
+			var url = "http://localhost:8000/multar/";
+
+			$.ajax( {
+				url:"http://192.168.1.106:8000/multar/" ,
+				type: "POST",
+				data: {
+    				placa:placa, 
+    				latitude:latitude,
+    				longitude:longitude,
+    				condutor:condutor,
+    				infracao:infracao,
+    				
+		    	},  
+		    	crossDomain: true,
+		    	success: function( data ) {
+					console.log( data );
+				}
+			});
+
+			$( document ).ajaxError(function( event, jqxhr, settings, exception ) {
+				console.log(jqxhr)
+				console.log(settings)
+				console.log(exception)
+			});
+
+/*
+		    $.post(url, {
+		    				placa:placa, 
+		    				latitude:latitude,
+		    				longitude:longitude,
+		    				condutor:condutor,
+		    				imagem:imagem,
+		    				infracao:infracao
+
+		    			}, 
+		    	function(response){
+
+			    	console.log(response);
+		    	});
+*/
+
+		});
+
+
+
+
+
 	},
 	onScroll: function (e) {
 		//checkscrol
