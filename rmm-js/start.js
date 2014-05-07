b6076d96ae160850871cb43c7c8bdc04efaf973b
Mayron Cachina <mayroncachina@gmail.com>
2014-05-07 18:07:15 -0300

    removendo checkconnection

--- rmm-js/start.js
+++ rmm-js/start.js
@@ -13,6 +13,5 @@ document.addEventListener("deviceready",onDeviceReady,false);
 function onDeviceReady() {
     pictureSource=navigator.camera.PictureSourceType;
     destinationType=navigator.camera.DestinationType;
-    alert('START DEVICE')
     checkConnection();
 }
